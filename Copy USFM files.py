423db1ee64f0514665c278351d1cd0c5dcf27b2f
Oleh Onyshchenko <seesmwork@gmail.com>
2024-10-25 16:16:40 +0300

    refactor: minor code tweaks in files copying `.py` file

--- Copy USFM files.py
+++ Copy USFM files.py
@@ -1,6 +1,5 @@
 import os
 from shutil import copy2
-teka=os.path.dirname(os.path.abspath(__file__))
-fajly=os.path.join(teka,"Standartna")
+fajly=os.path.join(os.path.dirname(os.path.abspath(__file__)),"Original")
 projekt=os.path.join("C:\\My Paratext 9 Projects\\BKS")
 for f in os.listdir(fajly): copy2(os.path.join(fajly,f),os.path.join(projekt,f))
