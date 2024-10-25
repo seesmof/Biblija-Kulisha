518aca6a039ab31b82ae33ee2751c6e68081f033
Oleh Onyshchenko <seesmwork@gmail.com>
2024-10-25 18:34:07 +0300

    feat: testing making text files in `.py` script: JESUS THANK  YOU LORD GOD ALMIGHTY

--- Copy USFM files.py
+++ Copy USFM files.py
@@ -1,5 +1,6 @@
 import os
 from shutil import copy2
+import re
 
 def get_relative_path(root_folder_path:str=os.path.dirname(os.path.abspath(__file__)),target_path:str=""):
     return os.path.join(root_folder_path,target_path)
@@ -8,7 +9,7 @@ ORIGINAL_FILES_PATH=get_relative_path("Original")
 TEXT_FILES_PATH=get_relative_path("Text")
 PARATEXT_PROJECT_PATH=get_relative_path("C:\\My Paratext 9 Projects\\BKS")
 
-def copy_files_to_paratext_project():
+def copy_original_to_paratext():
     for file in os.listdir(ORIGINAL_FILES_PATH):
         copy2(
             get_relative_path(ORIGINAL_FILES_PATH,file),
@@ -16,4 +17,31 @@ def copy_files_to_paratext_project():
         )
     print("All Bible original files copied to Paratext project")
 
-copy_files_to_paratext_project()
+# copy_original_to_paratext()
+
+def copy_original_to_text():
+    for file in os.listdir(ORIGINAL_FILES_PATH):
+        if file=='58PHMBKS.USFM':
+            with open(get_relative_path(ORIGINAL_FILES_PATH,file), mode='r',encoding='utf-8') as f:
+                lines=f.readlines()
+            lines=[
+                line for line in lines 
+                if '\\ide' not in line 
+                and '\\h' not in line 
+                and '\\toc3' not in line 
+                and '\\mt1' not in line 
+                and '\\p' not in line
+            ]
+            lines=[
+                line.replace(" - Biblija Kulisha Standartna","").replace("\\id ","###").replace("\\toc1","###!!").replace("\\toc2","###!").replace("\\c ","##").replace("\\v ","#") for line in lines
+            ]
+
+            file_name,file_extension=file.split(".")
+            file_name=file_name[2:].replace("BKS","")
+            file_extension="TXT"
+            file=f'{file_name}.{file_extension}'
+
+            with open(get_relative_path(TEXT_FILES_PATH,file),encoding='utf-8',mode='w') as f:
+                f.writelines(lines)
+
+copy_original_to_text()
