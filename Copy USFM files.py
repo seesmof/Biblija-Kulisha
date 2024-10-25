604dd66620cdeb1824d6075dd1e78e7b973d5da6
Oleh Onyshchenko <seesmwork@gmail.com>
2024-10-25 18:08:53 +0300

    feat: adding typos; refactor `.py` script, make more readable and modular, JESUS THANK YOU LORD GOD ALMIGHTY

--- Copy USFM files.py
+++ Copy USFM files.py
@@ -1,5 +1,19 @@
 import os
 from shutil import copy2
-fajly=os.path.join(os.path.dirname(os.path.abspath(__file__)),"Original")
-projekt=os.path.join("C:\\My Paratext 9 Projects\\BKS")
-for f in os.listdir(fajly): copy2(os.path.join(fajly,f),os.path.join(projekt,f))
+
+def get_relative_path(root_folder_path:str=os.path.dirname(os.path.abspath(__file__)),target_path:str=""):
+    return os.path.join(root_folder_path,target_path)
+
+ORIGINAL_FILES_PATH=get_relative_path("Original")
+TEXT_FILES_PATH=get_relative_path("Text")
+PARATEXT_PROJECT_PATH=get_relative_path("C:\\My Paratext 9 Projects\\BKS")
+
+def copy_files_to_paratext_project():
+    for file in os.listdir(ORIGINAL_FILES_PATH):
+        copy2(
+            get_relative_path(ORIGINAL_FILES_PATH,file),
+            get_relative_path(PARATEXT_PROJECT_PATH,file)
+        )
+    print("All Bible original files copied to Paratext project")
+
+copy_files_to_paratext_project()
