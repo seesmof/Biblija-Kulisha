330674fbcb061739c0a7dd404f4a54517c51bbf9
Oleh Onyshchenko <seesmwork@gmail.com>
2024-10-31 17:42:21 +0200

    refactor: py script handle fails when writing large text file test file's existance in test py script py script replace print with echo set default color for echo to none extract echo to global for using in all functions

--- Automations.py
+++ Automations.py
@@ -9,6 +9,7 @@ ORIGINAL_FILES_PATH=os.path.join(ROOT_PATH,"Original")
 ORIGINAL_FILES=glob.glob(ORIGINAL_FILES_PATH+"\\*.USFM")
 TEXT_FILES_PATH=os.path.join(ROOT_PATH,"Text")
 PARATEXT_PROJECT_PATH=os.path.join("C:\\My Paratext 9 Projects\\BKS")
+DEFAULT = '\033[0m'; BOLD = '\033[1m';ITALIC = '\033[3m';UNDERLINE = '\033[4m';UNDERLINE_THICK = '\033[21m';HIGHLIGHTED = '\033[7m';HIGHLIGHTED_BLACK = '\033[40m';HIGHLIGHTED_RED = '\033[41m';HIGHLIGHTED_GREEN = '\033[42m';HIGHLIGHTED_YELLOW = '\033[43m';HIGHLIGHTED_BLUE = '\033[44m';HIGHLIGHTED_PURPLE = '\033[45m';HIGHLIGHTED_CYAN = '\033[46m';HIGHLIGHTED_GREY = '\033[47m';HIGHLIGHTED_GREY_LIGHT = '\033[100m';HIGHLIGHTED_RED_LIGHT = '\033[101m';HIGHLIGHTED_GREEN_LIGHT = '\033[102m';HIGHLIGHTED_YELLOW_LIGHT = '\033[103m';HIGHLIGHTED_BLUE_LIGHT = '\033[104m';HIGHLIGHTED_PURPLE_LIGHT = '\033[105m';HIGHLIGHTED_CYAN_LIGHT = '\033[106m';HIGHLIGHTED_WHITE_LIGHT = '\033[107m';STRIKE_THROUGH = '\033[9m';MARGIN_1 = '\033[51m';MARGIN_2 = '\033[52m';BLACK = '\033[30m';RED_DARK = '\033[31m';GREEN_DARK = '\033[32m';YELLOW_DARK = '\033[33m';BLUE_DARK = '\033[34m';PURPLE_DARK = '\033[35m';CYAN_DARK = '\033[36m';GREY_DARK = '\033[37m';BLACK_LIGHT = '\033[90m';RED = '\033[91m';GREEN = '\033[92m';YELLOW = '\033[93m';BLUE = '\033[94m';PURPLE = '\033[95m';CYAN = '\033[96m';WHITE = '\033[97m';echo = lambda values, color=DEFAULT: print("%s%s%s" % (color, values, DEFAULT)) if color else print("%s%s" % (values, DEFAULT)) # source: https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal#:~:text=%2C%20color%3DCYAN)-,1%20Line,-Simply%20copy%20paste
 
 def copy_original_to_paratext():
     for full_file_name in os.listdir(ORIGINAL_FILES_PATH):
@@ -67,6 +68,7 @@ def make_single_text_file():
             words.append(word.upper() if quote else word)
             if '\\qt*' in word: quote=False
         return " ".join(words).replace("\\QT*","").replace("\\QT ","")
+
     global_lines=[]
     for full_file_name in os.listdir(ORIGINAL_FILES_PATH):
         with open(os.path.join(ORIGINAL_FILES_PATH,full_file_name),mode='r',encoding='utf-8') as f:
@@ -88,15 +90,20 @@ def make_single_text_file():
                     # handle Strong's numbers
                     verse=re.sub(r'\|strong=\"[GH]\d{4}\"\\w\*',"",verse).replace("\\w ","")
                     global_lines.append(f'{Book} {chapter}:{verse}')
-    with open(file=os.path.join(ROOT_PATH,"Original.txt"),encoding='utf-8',mode='w') as f:
-        f.writelines([
-            line+'\n' if index!=len(global_lines)-1 
-            else line 
-            for index,line in enumerate(global_lines)
-        ])
+
+    target_file_path=os.path.join(ROOT_PATH,'Original.txt')
+    try:
+        with open(target_file_path,encoding='utf-8',mode='w+') as f:
+            f.writelines([
+                line+'\n' 
+                if index != len(global_lines)-1 
+                else line 
+                for index, line in enumerate(global_lines)
+            ])
+    except:
+        echo("Failed to write single TXT file",color=YELLOW)
 
 def form_log_files():
-    DEFAULT = '\033[0m'; BOLD = '\033[1m';ITALIC = '\033[3m';UNDERLINE = '\033[4m';UNDERLINE_THICK = '\033[21m';HIGHLIGHTED = '\033[7m';HIGHLIGHTED_BLACK = '\033[40m';HIGHLIGHTED_RED = '\033[41m';HIGHLIGHTED_GREEN = '\033[42m';HIGHLIGHTED_YELLOW = '\033[43m';HIGHLIGHTED_BLUE = '\033[44m';HIGHLIGHTED_PURPLE = '\033[45m';HIGHLIGHTED_CYAN = '\033[46m';HIGHLIGHTED_GREY = '\033[47m';HIGHLIGHTED_GREY_LIGHT = '\033[100m';HIGHLIGHTED_RED_LIGHT = '\033[101m';HIGHLIGHTED_GREEN_LIGHT = '\033[102m';HIGHLIGHTED_YELLOW_LIGHT = '\033[103m';HIGHLIGHTED_BLUE_LIGHT = '\033[104m';HIGHLIGHTED_PURPLE_LIGHT = '\033[105m';HIGHLIGHTED_CYAN_LIGHT = '\033[106m';HIGHLIGHTED_WHITE_LIGHT = '\033[107m';STRIKE_THROUGH = '\033[9m';MARGIN_1 = '\033[51m';MARGIN_2 = '\033[52m';BLACK = '\033[30m';RED_DARK = '\033[31m';GREEN_DARK = '\033[32m';YELLOW_DARK = '\033[33m';BLUE_DARK = '\033[34m';PURPLE_DARK = '\033[35m';CYAN_DARK = '\033[36m';GREY_DARK = '\033[37m';BLACK_LIGHT = '\033[90m';RED = '\033[91m';GREEN = '\033[92m';YELLOW = '\033[93m';BLUE = '\033[94m';PURPLE = '\033[95m';CYAN = '\033[96m';WHITE = '\033[97m';echo = lambda values, color: print("%s%s%s" % (color, values, DEFAULT)) if color else print("%s%s" % (values, DEFAULT)) # source: https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal#:~:text=%2C%20color%3DCYAN)-,1%20Line,-Simply%20copy%20paste
     JESUS_Words=[]
     LORD_Names=[]
     OT_Quotes=[]
@@ -146,7 +153,7 @@ def monitor_files_for_changes():
     def get_modification_time(file:str):
         return os.path.getmtime(file)
 
-    print("Automations script started")
+    echo("Automations script started")
     perform_automations()
     latest_file=get_last_modified_file()
     last_modification_time=get_modification_time(latest_file)
--- a.py
+++ a.py
@@ -1,18 +1,7 @@
-# automatically log JESUS' Words into a text file with ranges
+# check if Original.txt file exists 
+import os
 
-import os 
-import glob 
 root=os.path.dirname(os.path.abspath(__file__))
-original=os.path.join(root,"Original")
-Bible_text_files=glob.glob(original+"\\*.USFM")
-
-for file_path in Bible_text_files:
-    with open(file_path,encoding="utf-8") as f:
-        lines=f.readlines()
-    Book_name=lines[2].replace("\\h ","").strip()
-    last_chapter=0
-    for line in lines:
-        if '\\c ' in line:
-            last_chapter=line[3:].strip()
-        if 'wj' in line:
-            print(Book_name,last_chapter,line[3:].split()[0])
+target=os.path.join(root,"Original.txt")
+with open(target,encoding='utf-8',mode='w+') as f:
+    f.writelines(["HALLELUJAH","AMEN"])
