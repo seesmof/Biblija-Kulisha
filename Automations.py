51937f04fb4c4577cb8a70485c79fb79a4267788
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-01 09:03:09 +0200

    refactor: py script add logging functions add exception handling on writing and reading files

--- Automations.py
+++ Automations.py
@@ -10,18 +10,33 @@ ORIGINAL_FILES=glob.glob(ORIGINAL_FILES_PATH+"\\*.USFM")
 TEXT_FILES_PATH=os.path.join(ROOT_PATH,"Text")
 PARATEXT_PROJECT_PATH=os.path.join("C:\\My Paratext 9 Projects\\BKS")
 DEFAULT = '\033[0m'; BOLD = '\033[1m';ITALIC = '\033[3m';UNDERLINE = '\033[4m';UNDERLINE_THICK = '\033[21m';HIGHLIGHTED = '\033[7m';HIGHLIGHTED_BLACK = '\033[40m';HIGHLIGHTED_RED = '\033[41m';HIGHLIGHTED_GREEN = '\033[42m';HIGHLIGHTED_YELLOW = '\033[43m';HIGHLIGHTED_BLUE = '\033[44m';HIGHLIGHTED_PURPLE = '\033[45m';HIGHLIGHTED_CYAN = '\033[46m';HIGHLIGHTED_GREY = '\033[47m';HIGHLIGHTED_GREY_LIGHT = '\033[100m';HIGHLIGHTED_RED_LIGHT = '\033[101m';HIGHLIGHTED_GREEN_LIGHT = '\033[102m';HIGHLIGHTED_YELLOW_LIGHT = '\033[103m';HIGHLIGHTED_BLUE_LIGHT = '\033[104m';HIGHLIGHTED_PURPLE_LIGHT = '\033[105m';HIGHLIGHTED_CYAN_LIGHT = '\033[106m';HIGHLIGHTED_WHITE_LIGHT = '\033[107m';STRIKE_THROUGH = '\033[9m';MARGIN_1 = '\033[51m';MARGIN_2 = '\033[52m';BLACK = '\033[30m';RED_DARK = '\033[31m';GREEN_DARK = '\033[32m';YELLOW_DARK = '\033[33m';BLUE_DARK = '\033[34m';PURPLE_DARK = '\033[35m';CYAN_DARK = '\033[36m';GREY_DARK = '\033[37m';BLACK_LIGHT = '\033[90m';RED = '\033[91m';GREEN = '\033[92m';YELLOW = '\033[93m';BLUE = '\033[94m';PURPLE = '\033[95m';CYAN = '\033[96m';WHITE = '\033[97m';echo = lambda values, color=DEFAULT: print("%s%s%s" % (color, values, DEFAULT)) if color else print("%s%s" % (values, DEFAULT)) # source: https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal#:~:text=%2C%20color%3DCYAN)-,1%20Line,-Simply%20copy%20paste
+log=lambda text:echo(text,color=ITALIC)
+yes=lambda section,text:echo(f"{section.upper()}: {text}",color=GREEN_DARK)
+fail=lambda section,text:echo(f"{section.upper()}: fail {text}",color=RED_DARK)
+warn=lambda section,text:echo(f"{section.upper()}: {text}",color=BOLD)
 
 def copy_original_to_paratext():
-    for full_file_name in os.listdir(ORIGINAL_FILES_PATH):
-        copy2(
-            os.path.join(ORIGINAL_FILES_PATH,full_file_name),
-            os.path.join(PARATEXT_PROJECT_PATH,full_file_name)
-        )
+    section="PARA"
+    try:
+        for full_file_name in os.listdir(ORIGINAL_FILES_PATH):
+            copy2(
+                os.path.join(ORIGINAL_FILES_PATH,full_file_name),
+                os.path.join(PARATEXT_PROJECT_PATH,full_file_name)
+            )
+    except: 
+        fail(section,"copying files")
+    yes(section,"copy files")
 
 def copy_original_to_text():
+    section="TEXT"
     for full_file_name in os.listdir(ORIGINAL_FILES_PATH):
-        with open(os.path.join(ORIGINAL_FILES_PATH,full_file_name), mode='r',encoding='utf-8') as f:
-            lines=f.readlines()
+        try:
+            target_file_path=os.path.join(ORIGINAL_FILES_PATH,full_file_name)
+            with open(file=target_file_path,encoding='utf-8',mode='r') as f:
+                lines=f.readlines()
+        except:
+            fail(section,f"reading {full_file_name}")
+        
         lines=[
             line for line in lines 
             if '\\ide' not in line 
@@ -56,8 +71,14 @@ def copy_original_to_text():
         file_extension="TXT"
         full_file_name=f'{file_name}.{file_extension}'
 
-        with open(os.path.join(TEXT_FILES_PATH,full_file_name),encoding='utf-8',mode='w') as f:
-            f.writelines(lines)
+        try:
+            target_file_path=os.path.join(TEXT_FILES_PATH,full_file_name)
+            with open(file=target_file_path,encoding='utf-8',mode='w') as f:
+                f.writelines(lines)
+        except:
+            fail(section,f"writing {full_file_name}")
+    yes(section,f"form files")
+
 
 def make_single_text_file():
     def handle_quotes(verse:str):
@@ -86,31 +107,37 @@ def make_single_text_file():
                 words.append(word)
         return " ".join(words)
 
+    section="HUGE"
     global_lines=[]
     for full_file_name in os.listdir(ORIGINAL_FILES_PATH):
-        with open(os.path.join(ORIGINAL_FILES_PATH,full_file_name),mode='r',encoding='utf-8') as f:
-            current_lines=f.readlines()
-            Book=current_lines[2].replace("\\h ","").strip()
-            chapter=1
-            for line in current_lines:
-                if '\\c ' in line:
-                    chapter=line[3:].strip()
-                elif '\\v ' in line:
-                    # remove verse tag
-                    verse=line[3:].strip()
-                    # remove WJ tags 
-                    verse=verse.replace("\\wj*","").replace("\\wj ","")
-                    # handle ND tags 
-                    verse="".join(m.upper() if ' ' not in m else m for m in re.split(r'\\nd (.*?)\\nd\*',verse))
-                    # handle QT tags
-                    verse=handle_quotes(verse)
-                    # handle Strong's numbers
-                    verse=re.sub(r'\|strong=\"[GH]\d{4}\"\\w\*',"",verse).replace("\\w ","")
-                    global_lines.append(f'{Book} {chapter}:{verse}')
+        try:
+            target_file_path=os.path.join(ORIGINAL_FILES_PATH,full_file_name)
+            with open(os.path.join(ORIGINAL_FILES_PATH,full_file_name),encoding='utf-8',mode='r') as f:
+                current_lines=f.readlines()
+        except:
+            fail(section,f"reading {full_file_name}")
 
-    target_file_path=os.path.join(ROOT_PATH,'Original.txt')
+        Book=current_lines[2].replace("\\h ","").strip()
+        chapter=1
+        for line in current_lines:
+            if '\\c ' in line:
+                chapter=line[3:].strip()
+            elif '\\v ' in line:
+                # remove verse tag
+                verse=line[3:].strip()
+                # remove WJ tags 
+                verse=verse.replace("\\wj*","").replace("\\wj ","")
+                # handle ND tags 
+                verse="".join(m.upper() if ' ' not in m else m for m in re.split(r'\\nd (.*?)\\nd\*',verse))
+                # handle QT tags
+                verse=handle_quotes(verse)
+                # handle Strong's numbers
+                verse=re.sub(r'\|strong=\"[GH]\d{4}\"\\w\*',"",verse).replace("\\w ","")
+                global_lines.append(f'{Book} {chapter}:{verse}')
+
     try:
-        with open(target_file_path,encoding='utf-8',mode='w+') as f:
+        target_file_path=os.path.join(ROOT_PATH,'Original.txt')
+        with open(file=target_file_path,encoding='utf-8',mode='w+') as f:
             f.writelines([
                 line+'\n' 
                 if index != len(global_lines)-1 
@@ -118,15 +145,21 @@ def make_single_text_file():
                 for index, line in enumerate(global_lines)
             ])
     except:
-        echo("Failed to write single TXT file",color=YELLOW)
+        fail(section,"forming file")
+    yes(section,"form file")
 
 def form_log_files():
     JESUS_Words=[]
     LORD_Names=[]
     OT_Quotes=[]
+    section="LOGS"
     for file_path in ORIGINAL_FILES:
-        with open(file_path,encoding='utf-8',mode='r') as f:
-            lines=f.readlines()
+        try:
+            with open(file_path,encoding='utf-8',mode='r') as f:
+                lines=f.readlines()
+        except:
+            fail(section,f"reading {file_path.split("\\")[-1]}")
+
         Book_name=lines[2].replace("\\h ","").strip()
         last_chapter=0
         for line in lines:
@@ -134,29 +167,41 @@ def form_log_files():
                 last_chapter=line[3:].strip()
             if '\\wj' in line:
                 verse_reference=f"{Book_name} {last_chapter}:{line[3:].split()[0]}"
-                if '*' not in line: 
-                    echo(f"WARNING: Missing closing tag {verse_reference}",color=RED_DARK)
-                    exit()
+                if '*' not in line:
+                    warn(section,f"{verse_reference} missing closing WJ tag")
                 JESUS_Words.append(verse_reference)
             if '\\nd' in line:
                 verse_reference=f"{Book_name} {last_chapter}:{line[3:].split()[0]}"
-                if '*' not in line: 
-                    echo(f"WARNING: Missing closing tag {verse_reference}",color=RED_DARK)
-                    exit()
+                if '*' not in line:
+                    warn(section,f"{verse_reference} missing closing ND tag")
                 LORD_Names.append(verse_reference)
             if '\\qt' in line:
                 verse_reference=f"{Book_name} {last_chapter}:{line[3:].split()[0]}"
-                if '*' not in line: 
-                    echo(f"WARNING: Missing closing tag {verse_reference}",color=RED_DARK)
-                    exit()
+                if '*' not in line:
+                    warn(section,f"{verse_reference} missing closing QT tag")
                 OT_Quotes.append(verse_reference)
 
-    with open(os.path.join(ROOT_PATH,"JESUS_Words.txt"),encoding='utf-8',mode='w') as f:
-        f.write("\n".join(JESUS_Words))
-    with open(os.path.join(ROOT_PATH,"LORD_Names.txt"),encoding='utf-8',mode='w') as f:
-        f.write("\n".join(LORD_Names))
-    with open(os.path.join(ROOT_PATH,"OT_Quotes.txt"),encoding='utf-8',mode='w') as f:
-        f.write("\n".join(OT_Quotes))
+    JESUS_Words_target_path=os.path.join(ROOT_PATH,"JESUS_Words.txt")
+    try:
+        with open(file=JESUS_Words_target_path,encoding='utf-8',mode='w') as f:
+            f.write("\n".join(JESUS_Words))
+    except:
+        fail(section,"forming JESUS' Words file")
+    
+    LORD_Names_target_path=os.path.join(ROOT_PATH,"LORD_Names.txt")
+    try:
+        with open(os.path.join(ROOT_PATH,"LORD_Names.txt"),encoding='utf-8',mode='w') as f:
+            f.write("\n".join(LORD_Names))
+    except:
+        fail(section,"forming LORD's Names file")
+
+    try:
+        target_file_path=os.path.join(ROOT_PATH,"OT_Quotes.txt")
+        with open(file=target_file_path,encoding='utf-8',mode='w') as f:
+            f.write("\n".join(OT_Quotes))
+    except:
+        fail(section,"forming OT Quotes file")
+    yes(section,"form files")
 
 def perform_automations():
     copy_original_to_paratext()
@@ -170,17 +215,20 @@ def monitor_files_for_changes():
     def get_modification_time(file:str):
         return os.path.getmtime(file)
 
-    echo("Automations script started")
-    perform_automations()
+    echo("START\n")
     latest_file=get_last_modified_file()
     last_modification_time=get_modification_time(latest_file)
+    echo(time.ctime(last_modification_time))
+    perform_automations()
     while 1:
         latest_file=get_last_modified_file()
         current_modification_time=get_modification_time(latest_file)
         if last_modification_time!=current_modification_time:
+            print()
+            echo(time.ctime(last_modification_time))
             perform_automations()
             last_modification_time=current_modification_time
-            # print(latest_file.split("\\")[-1][2:5],time.ctime(last_modification_time))
         time.sleep(1)
 
 monitor_files_for_changes()
+# TODO add retrying when writing or reading files
