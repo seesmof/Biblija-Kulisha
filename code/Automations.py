36ca55e2059a93216aa6af51c7e31e610ec8b1fd
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-08 11:30:41 +0200

    feat: py script add solid and TBS files forming to Output

--- code/Automations.py
+++ code/Automations.py
@@ -7,7 +7,9 @@ from shutil import copy2
 ROOT_PATH=os.path.join(os.path.dirname(os.path.abspath(__file__)),"..")
 ORIGINAL_FILES_PATH=os.path.join(ROOT_PATH,"Original")
 ORIGINAL_FILES=glob.glob(ORIGINAL_FILES_PATH+"\\*.USFM")
-TEXT_FILES_PATH=os.path.join(ROOT_PATH,"Text")
+OUTPUT_FOLDER_PATH=os.path.join(ROOT_PATH,'Output')
+TEXT_TBS_FOLDER_PATH=os.path.join(OUTPUT_FOLDER_PATH,'TXT TBS')
+TEXT_SOLID_FOLDER_PATH=os.path.join(OUTPUT_FOLDER_PATH,'TXT SLD')
 LOG_FILES_PATH=os.path.join(ROOT_PATH,"logs")
 PARATEXT_PROJECT_PATH=os.path.join("C:\\My Paratext 9 Projects\\BKS")
 DEFAULT = '\033[0m'; BOLD = '\033[1m';ITALIC = '\033[3m';UNDERLINE = '\033[4m';UNDERLINE_THICK = '\033[21m';HIGHLIGHTED = '\033[7m';HIGHLIGHTED_BLACK = '\033[40m';HIGHLIGHTED_RED = '\033[41m';HIGHLIGHTED_GREEN = '\033[42m';HIGHLIGHTED_YELLOW = '\033[43m';HIGHLIGHTED_BLUE = '\033[44m';HIGHLIGHTED_PURPLE = '\033[45m';HIGHLIGHTED_CYAN = '\033[46m';HIGHLIGHTED_GREY = '\033[47m';HIGHLIGHTED_GREY_LIGHT = '\033[100m';HIGHLIGHTED_RED_LIGHT = '\033[101m';HIGHLIGHTED_GREEN_LIGHT = '\033[102m';HIGHLIGHTED_YELLOW_LIGHT = '\033[103m';HIGHLIGHTED_BLUE_LIGHT = '\033[104m';HIGHLIGHTED_PURPLE_LIGHT = '\033[105m';HIGHLIGHTED_CYAN_LIGHT = '\033[106m';HIGHLIGHTED_WHITE_LIGHT = '\033[107m';STRIKE_THROUGH = '\033[9m';MARGIN_1 = '\033[51m';MARGIN_2 = '\033[52m';BLACK = '\033[30m';RED_DARK = '\033[31m';GREEN_DARK = '\033[32m';YELLOW_DARK = '\033[33m';BLUE_DARK = '\033[34m';PURPLE_DARK = '\033[35m';CYAN_DARK = '\033[36m';GREY_DARK = '\033[37m';BLACK_LIGHT = '\033[90m';RED = '\033[91m';GREEN = '\033[92m';YELLOW = '\033[93m';BLUE = '\033[94m';PURPLE = '\033[95m';CYAN = '\033[96m';WHITE = '\033[97m';echo = lambda values, color=DEFAULT: print("%s%s%s" % (color, values, DEFAULT)) if color else print("%s%s" % (values, DEFAULT)) # source: https://stackoverflow.com/questions/287871/how-do-i-print-colored-text-to-the-terminal#:~:text=%2C%20color%3DCYAN)-,1%20Line,-Simply%20copy%20paste
@@ -34,6 +36,7 @@ def write_file(
     root:str=ROOT_PATH,
     tries:int=0
 ):
+    # TODO remove this
     if tries>=3:
         fail(section,f"writing {file_name}")
         return
@@ -45,6 +48,7 @@ def write_file(
         write_file(section,file_name,lines,root,tries+1)
 
 def combine_lines(lines:list):
+    # TODO figure out why this is here
     return [
         f'{line}\n' if line_index!=len(lines)-1 else line for line_index,line in enumerate(lines)
     ]
@@ -166,10 +170,10 @@ def form_logs():
     write_file(section,"F.csv",combine_lines(F),LOG_FILES_PATH)
     yes(section,"form files")
 
-def form_text_tbs(source_path:str=ORIGINAL_FILES_PATH):
+def form_text_tbs():
     section="TEXT"
-    for full_file_name in os.listdir(source_path):
-        target_file_path=os.path.join(source_path,full_file_name)
+    for full_file_name in os.listdir(ORIGINAL_FILES_PATH):
+        target_file_path=os.path.join(ORIGINAL_FILES_PATH,full_file_name)
         with open(file=target_file_path,encoding='utf-8',mode='r') as f:
             lines=f.readlines()
         
@@ -225,30 +229,37 @@ def form_text_tbs(source_path:str=ORIGINAL_FILES_PATH):
         file_extension="TXT"
         full_file_name=f'{file_name}.{file_extension}'
 
-        write_file(section,full_file_name,lines,TEXT_FILES_PATH)
+        write_file(section,full_file_name,lines,TEXT_TBS_FOLDER_PATH)
     yes(section,f"form files")
 
 def form_text_solid():
-    gls=[]
-    for f in ORIGINAL_FILES:
-        with open(f,encoding='utf-8',mode='r') as fr:
-            ls=fr.readlines()
-        ls=[
-            re.sub(r'\\v\s\d+\s','',l).replace("\\wj*","").replace("\\wj ","").replace("\\+wj*","").replace("\\+wj ","").replace("\\nd*","").replace("\\nd ","").replace("\\+nd*","").replace("\\+nd ","").replace("\\qt*","").replace("\\qt ","").replace("\\+qt*","").replace("\\+qt ","")
-            for l in ls
-            if '\\p' not in l
-            and '\\c' not in l
-            and '\\id' not in l
-            and '\\h' not in l
-            and '\\toc1' not in l
-            and '\\toc2' not in l
-            and '\\toc3' not in l
-            and '\\mt1' not in l
+    all_lines=[]
+    for file_path in ORIGINAL_FILES:
+        with open(file_path,encoding='utf-8',mode='r') as filer:
+            lines=filer.readlines()
+        lines=[
+            remove_usfm_tags(
+                # Match verse tags and numbers and remove them
+                re.sub(r'\\v\s\d+\s','',line)
+            )
+            for line in lines
+            if '\\p' not in line
+            and '\\c' not in line
+            and '\\id' not in line
+            and '\\h' not in line
+            and '\\toc1' not in line
+            and '\\toc2' not in line
+            and '\\toc3' not in line
+            and '\\mt1' not in line
         ]
-        gls+=ls
-    res=" ".join([l.replace('\n','') for l in gls])
-    with open(os.path.join(ROOT_PATH,"Solid.txt"),encoding='utf-8',mode='w') as fr:
-        fr.write(res)
+        all_lines+=lines
+    # Join everything into one solid wall of text, ALLELUJAH JESUS THANK YOU LORD GOD ALMIGHTY!
+    res=" ".join(
+        # Replace all new line tags
+        [line.replace('\n','') for line in all_lines]
+    )
+    with open(os.path.join(TEXT_SOLID_FOLDER_PATH,'Solid.txt'),encoding='utf-8',mode='w') as filer:
+        filer.write(res)
 
 def perform_automations(last_time):
     print()
@@ -256,8 +267,8 @@ def perform_automations(last_time):
 
     copy_to_paratext()
     form_text_tbs()
+    form_text_solid()
     form_logs()
-    # make_single_text_file()
 
 def monitor_files_for_changes():
     def get_last_modified_file():
