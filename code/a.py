f241d1ee96c575a665863f0f90c856837e511784
Oleh Onyshchenko <seesmwork@gmail.com>
2024-11-09 20:19:02 +0200

    fix: changes csv fix ACT 22 2 verse number

--- code/a.py
+++ code/a.py
@@ -1,3 +1,9 @@
-a='ZEC 1 5 wrong dash'
-Book,chapter,verse,reason=a.split(' ',3)
-print(Book,chapter,verse,reason.capitalize())
+import glob
+import os
+
+
+root=os.path.join(os.path.dirname(os.path.abspath(__file__)),"..")
+original_folder_path=os.path.join(root,"Original")
+original_file_paths=glob.glob(original_folder_path+"\\*.USFM")
+
+a=[]
