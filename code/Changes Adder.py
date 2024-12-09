a2255f25c247313a9f313aaaf48068b837f1431f
Oleh Onyshchenko <seesmwork@gmail.com>
2024-12-09 23:28:32 +0200

    Code validate form before sending

--- code/Changes Adder.py
+++ code/Changes Adder.py
@@ -14,6 +14,15 @@ def reset_local_storage():
 
 
 def add_new_change_entry(changes_file_path: str):
+    if (
+        app.storage.general["Book"] == ""
+        or app.storage.general["Chapter"] == ""
+        or app.storage.general["Verse"] == ""
+        or app.storage.general["Mistake"] == ""
+        or app.storage.general["Correction"] == ""
+        or app.storage.general["Reason"] == ""
+    ):
+        return
     entry_line = f"| {app.storage.general['Book']} | {app.storage.general['Chapter']} | {app.storage.general['Verse']} | {app.storage.general['Mistake']} | {app.storage.general['Correction']} | {app.storage.general['Reason']} |"
     with open(changes_file_path, encoding="utf-8", mode="a") as f:
         f.write("\n" + entry_line)
@@ -25,21 +34,53 @@ changes_file_path = os.path.join(root_folder, "..", "docs", "Checks", "Changes.m
 reasons_autocomplete = ["wrong", "missing", "letter", "symbol"]
 
 Book_select = ui.select(
+    validation={"Cannot be empty": bool},
     label="Book",
     options=Ukrainian_Bible_Book_name_to_English_abbrevation,
     with_input=True,
 ).bind_value(app.storage.general, "Book")
-Chapter_input = ui.input(label="Chapter").bind_value(app.storage.general, "Chapter")
-Verse_input = ui.input(label="Verse").bind_value(app.storage.general, "Verse")
-Mistake_input = ui.input(label="Mistake").bind_value(app.storage.general, "Mistake")
-Correction_input = ui.input(label="Correction").bind_value(
-    app.storage.general, "Correction"
+Chapter_input = ui.input(
+    validation={"Cannot be empty": bool}, label="Chapter"
+).bind_value(app.storage.general, "Chapter")
+Verse_input = ui.input(validation={"Cannot be empty": bool}, label="Verse").bind_value(
+    app.storage.general, "Verse"
 )
-Reason_input = ui.input(label="Reason", autocomplete=reasons_autocomplete).bind_value(
-    app.storage.general, "Reason"
+Mistake_input = ui.input(
+    validation={"Cannot be empty": bool}, label="Mistake"
+).bind_value(app.storage.general, "Mistake")
+Correction_input = ui.input(
+    validation={"Cannot be empty": bool}, label="Correction"
+).bind_value(app.storage.general, "Correction")
+Reason_input = ui.input(
+    validation={"Cannot be empty": bool},
+    label="Reason",
+    autocomplete=reasons_autocomplete,
+).bind_value(app.storage.general, "Reason")
+
+
+class FormValidator:
+    def __init__(self, *inputs):
+        self.elements = inputs
+
+    @property
+    def correct(self):
+        return all(
+            validation(element.value)
+            for element in self.elements
+            for validation in element.validation.values()
+        )
+
+
+form_validator = FormValidator(
+    Book_select,
+    Chapter_input,
+    Verse_input,
+    Mistake_input,
+    Correction_input,
+    Reason_input,
 )
 send_button = ui.button(
     "Make", on_click=lambda: add_new_change_entry(changes_file_path)
-)
+).bind_enabled_from(form_validator, "correct")
 
 ui.run(favicon="🏖️", title="Add new Bible change")
