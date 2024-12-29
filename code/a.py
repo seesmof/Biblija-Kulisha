23b8ba45517d29df16a0b559966d9fa704cd1368
Oleh Onyshchenko <seesmwork@gmail.com>
2024-12-29 15:29:08 +0200

    code automations file correctly form TBS text files

--- code/a.py
+++ code/a.py
@@ -2,6 +2,7 @@ import shutil
 import time
 import util
 import os
+import re
 
 '''
 copy Original and Revision files to their paratext projects 
@@ -25,17 +26,33 @@ def copy_files_to_paratext_project(
                 lines=f.readlines()
             lines=[l for l in lines if not l.startswith(r'\rem ')]
             with open(paratext_file_path,encoding='utf-8',mode='w') as f:
-                f.write('\n'.join(lines))
+                f.write('\n'.join([l.strip() for l in lines]))
 
 
 def make_tbs_text_files(
     folder_path:str=util.original_folder_path,
 ):
     for file_name in os.listdir(folder_path):
+        Book_name=file_name[2:].replace('.USFM','')
         file_path=os.path.join(folder_path,file_name)
         with open(file_path,encoding='utf-8',mode='r') as f:
             lines=f.readlines()
-        
+
+        needed_tags=['toc','v','s','c']
+        lines=[f'###{Book_name}']+[
+            re.sub(r'\\f\s\+\s\\ft\s','[',line).replace(r'\f*',']')
+            .replace(r'\v ','#')
+            .replace(r'\c ','##')
+            .replace(r'\toc2','###!')
+            .replace(r'\toc1','###!!')
+            for line in lines 
+            if any(tag in line for tag in needed_tags)
+        ]
+    
+        output_file_path=os.path.join(util.docs_folder_path,'Original','TBS',file_name[2:].replace('USFM','TXT'))
+        with open(output_file_path,encoding='utf-8',mode='w') as f:
+            f.write('\n'.join([l.strip() for l in lines]))
+
 
 
 def perform_automations():
