d3422f0dc13fc8537d1cdc0d966a575d905da0ab
Oleh Onyshchenko <seesmwork@gmail.com>
2024-12-29 11:22:10 +0200

    code consts replace paratext project folder path with general paratext projects folder path

--- code/util/consts.py
+++ code/util/consts.py
@@ -2,4 +2,4 @@ import os
 
 root_folder_path=os.path.join(os.path.dirname(os.path.abspath(__file__)),'..','..')
 original_folder_path=os.path.join(root_folder_path,'Original')
-original_paratext_folder_path=os.path.join(r'C:\My Paratext 9 Projects\BKS')
+paratext_projects_folder_path=os.path.join(r'C:\My Paratext 9 Projects')
