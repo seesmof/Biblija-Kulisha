a82027aa4bb42fe45ac5ce778fdf4b5cac2bf2f9
Oleh Onyshchenko <seesmwork@gmail.com>
2025-03-05 09:48:25 +0200

    attempted to load Bible json

--- code/a.py
+++ code/a.py
@@ -1,33 +1,14 @@
-import util
-import os
-import re
+from collections import defaultdict
+import json
+file_path=r"E:\Biblija-Kulisha\docs\Original\UBK.json"
 
-def make_solid_file(
-    source_folder_path = util.original_folder_path,
-    vault_output_file_path = r'E:\Notatnyk\Біблія Куліша.log',
-):
-    output=[]
-    for file_name in os.listdir(source_folder_path):
-        if 'FRT' in file_name: continue
-        res=[]
-        file_path=os.path.join(source_folder_path,file_name)
-        lines=util.read_file_lines(file_path)
-        avoided_tags='h,toc,c,p,id,mt,s'
-        avoided_tags=avoided_tags.split(',')
-        cleared_lines=[l for l in lines if not any(t in l for t in avoided_tags)]
-        for l in cleared_lines:
-            v,c=l[3:].split(' ',maxsplit=1)
-            res.append(c.strip())
-        res=' '.join(res)
-        output.append(res)
-    output=' '.join(output)
-        
-    try:
-        if 'Notatnyk' not in vault_output_file_path:
-            vault_output_file_path=os.path.join(r'E:\Notatnyk',vault_output_file_path)
-        with open(vault_output_file_path,encoding='utf-8',mode='w') as f:
-            f.write(output.strip())
-    except: pass
+with open(file_path,encoding='utf-8',mode='r') as f:
+    loaded_dictionary: dict = json.load(f)
 
-make_solid_file(source_folder_path=util.original_folder_path,vault_output_file_path=r'E:\Notatnyk\Біблія Куліша.log')
-print('Solid Original')
+numbered_dictionary: dict = defaultdict(dict)
+for Book_number,Book_contents in loaded_dictionary.items():
+    Book_number=int(Book_number)
+    numbered_dictionary[Book_number]=dict()
+    for chapter_number,chapter_contents in loaded_dictionary[Book_number].items():
+        chapter_number=int(chapter_number)
+        numbered_dictionary[Book_number]
