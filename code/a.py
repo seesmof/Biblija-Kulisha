3a8b5eb89cf9e8f6bd32610ad3bad4d61d268488
Oleg Onyshchenko <seesmwork@gmail.com>
2026-08-31 18:08:53 +0300

    chore: reorganize the original docs files better, remove redundant files

--- code/a.py
+++ code/a.py
@@ -1,33 +1,7 @@
 import os
 
-import util
+paratext_project_folder_path: str = os.path.join("C:\\My Paratext 9 Projects\\UFB")
 
-source_folder_path: str = util.original_folder_path
-output_lines = []
-for file_name in os.listdir(source_folder_path):
-    file_path = os.path.join(source_folder_path, file_name)
-    lines = util.read_file_lines(file_path)
-    Book_name = util.get_Book_name_from_full_file_name(file_name)
-    chapter_number = 1
-
-    for line in lines:
-        if "\\c " in line:
-            chapter_number = line[3:].split()[0]
-        elif r"\v " in line:
-            verse_text = line[3:].strip()
-            stripped_formatting_tags = util.remove_formatting_usfm_tags(verse_text)
-            removed_footnotes = util.remove_footnotes_and_crossreferences_with_contents(
-                stripped_formatting_tags
-            )
-            removed_strongs_numbres = (
-                util.remove_strongs_numbers(removed_footnotes)
-                .strip()
-                .replace("  ", " ")
-            )
-            line = f"{Book_name} {chapter_number}:{removed_strongs_numbres}"
-            output_lines.append(line)
-
-print("\n".join(output_lines))
-file_path = os.path.join(util.original_folder_path, "..", "Bible.txt")
-with open(file_path, encoding="utf-8", mode="w") as f:
-    f.write("\n".join(output_lines))
+for file_name in os.listdir(paratext_project_folder_path):
+    if ".usfm" not in file_name.lower() or ".bak" in file_name.lower():
+        continue
